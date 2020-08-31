471fcbd54b888acfe96ca84bd35594090ae33cd2
LizzyKaiel <70513607+LizzyKaiel@users.noreply.github.com>
2020-08-31 13:10:55 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,3 @@
+print("Hej, Jag heter Elizabeth och jag är född och uppväxt i Gbg.")
+print("Jag har testat python (för nåt år sedan) men preparantkursen var första gången jag skrev ett litet program.")
+print("Jag var lite inne på att bli testare och så hittade jag KYH när jag kollade runt på nätet. Dem såg bra ut så jag sökte. ")
