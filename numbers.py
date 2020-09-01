ac9a72af14a58fb318fcc7815f5ded80ed6851ad
LizzyKaiel <Teiga96poke>
2020-09-01 13:10:40 +0200

    Added mainloop

--- numbers.py
+++ numbers.py
@@ -1,23 +1,26 @@
-import random
+def mainloop():
+    import random
 
-n = random.randint(1, 100)
-print("Jag tänker på ett nummer mellan 1 och 100. Gissa vilket!")
+    n = random.randint(1, 100)
+    print("Jag tänker på ett nummer mellan 1 och 100. Gissa vilket!")
 
-antal_gissningar = 1
+    antal_gissningar = 1
 
-while True:
-    text = input("Din gissning: ")
-    as_number = int(text)
+    while True:
+        text = input("Din gissning: ")
+        as_number = int(text)
 
-    if as_number == n:
-        print("Rätt!")
-        print("Så här många gånger försökte du:", antal_gissningar)
-        break
+        if as_number == n:
+            print("Rätt!")
+            print("Så här många gånger försökte du:", antal_gissningar)
+            break
 
-    if as_number < n:
-        print("Fel! Mitt nummer är högre... Testa igen!")
-        antal_gissningar= antal_gissningar + 1
+        if as_number < n:
+            print("Fel! Mitt nummer är högre... Testa igen!")
+            antal_gissningar= antal_gissningar + 1
 
-    if as_number > n:
-        print("Fel! Mitt nummer är lägre... Testa igen!")
-        antal_gissningar= antal_gissningar + 1
+        if as_number > n:
+            print("Fel! Mitt nummer är lägre... Testa igen!")
+            antal_gissningar= antal_gissningar + 1
+
+mainloop()
