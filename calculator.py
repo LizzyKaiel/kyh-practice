0f91ce066dba38f9be4cd3184aa3e02830846811
LizzyKaiel <Teiga96poke>
2020-09-02 09:30:05 +0200

    Added multiply and divide to choose from

--- calculator.py
+++ calculator.py
@@ -18,6 +18,8 @@ def run():
     print("This program will help with simple calculations. What do you want to do?")
     print("1 - add numbers")
     print("2 - subtract numbers")
+    print("3 - multiply numbers")
+    print("4 - divide numbers")
     answer = input(">> ")
     a = int(input("A="))
     b = int(input("B="))
