310d3e9d5ba9092711f63db6b9c478b3a08e237f
LizzyKaiel <Teiga96poke>
2020-09-07 10:36:07 +0200

    input. Add own name to the list

--- ls.py
+++ ls.py
@@ -2,7 +2,7 @@ def is_it_too_long(name):
     return len(name) > 5
 
 def main():
-    students = ["anna", "beatrice", "cecilia", "doris", "esmeralda", "frida", "gunilla"]
+    students = input("Skriv in name med kommatecken:").split()
     for name in students:
      if is_it_too_long(name):
            print(f"{name} är för långt!")
