949ece5fce1530b0eb705f546b90b43981ff0dab
LizzyKaiel <Teiga96poke>
2020-09-07 11:40:33 +0200

    Make name first letter uppercase when printed. Plus added ',' in split

--- ls.py
+++ ls.py
@@ -2,10 +2,10 @@ def is_it_too_long(name):
     return len(name) > 5
 
 def main():
-    students = input("Skriv in name med kommatecken:").split()
+    students = input("Skriv in name med kommatecken:").split(',')
     for name in students:
      if is_it_too_long(name):
-           print(f"{name.upper()} är för långt!")
+           print(f"{name.title()} är för långt!")
 
 
 if __name__ == '__main__':
