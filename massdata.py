401e37df5c8278d3703602d672fd0c661344553f
LizzyKaiel <Teiga96poke>
2020-09-14 23:33:05 +0200

    added total sum for all things.

--- massdata.py
+++ massdata.py
@@ -8,13 +8,12 @@ def main():
 
 
     sumValue1 = sum(d["item"] for d in o["entries"] if d)
-  # sumValue2 = sum(d['spec'] for d in o["entries"] if d)
-  # sumValue3 = sum(d['total'] for d in o["entries"] if d)
+    sumValue2 = sum(d['spec'] for d in o["entries"] if d)
+    sumValue3 = sum(d['total'] for d in o["entries"] if d)
 
-
-
-
-    print(sumValue1)
+    print(f"Total sum for items: {sumValue1}\n"
+          f"Total sum for spec: {sumValue2}\n"
+          f"Total sum for total: {sumValue3} ")
 
 
 if __name__ == '__main__':
