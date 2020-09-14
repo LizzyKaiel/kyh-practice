74c4b92366b699a61c809d4dc00dea4c1708952f
LizzyKaiel <Teiga96poke>
2020-09-14 23:38:18 +0200

    added the total value of everything

--- massdata.py
+++ massdata.py
@@ -6,15 +6,13 @@ def main():
     s = p.read_text(encoding="utf8")
     o = json.loads(s)
 
-
     sumValue1 = sum(d["item"] for d in o["entries"] if d)
     sumValue2 = sum(d['spec'] for d in o["entries"] if d)
     sumValue3 = sum(d['total'] for d in o["entries"] if d)
+    total = sumValue1 + sumValue2 + sumValue3
 
-    print(f"Total sum for items: {sumValue1}\n"
-          f"Total sum for spec: {sumValue2}\n"
-          f"Total sum for total: {sumValue3} ")
-
+    print(f"Total sum for items: {sumValue1}\nTotal sum for spec: {sumValue2}\nTotal sum for total: {sumValue3} ")
+    print(f"Everything in total: {total}")
 
 if __name__ == '__main__':
     main()
